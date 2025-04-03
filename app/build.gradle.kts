cae04625f2260b275e373b07415915ff0351c89e
Mary Janelle <maryyyjanellee0616@gmail.com>
2025-04-03 22:47:35 +0800

    commit and push

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,6 +2,7 @@ import com.android.tools.r8.internal.fr
 
 plugins {
     alias(libs.plugins.android.application)
+    alias(libs.plugins.google.gms.google.services)
 }
 
 android {
@@ -10,7 +11,7 @@ android {
 
     defaultConfig {
         applicationId = "com.example.basuratrack"
-        minSdk = 21
+        minSdk = 23
         targetSdk = 34
         versionCode = 1
         versionName = "1.0"
@@ -45,6 +46,7 @@ dependencies {
     implementation(libs.material)
     implementation(libs.activity)
     implementation(libs.constraintlayout)
+    implementation(libs.firebase.auth)
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
